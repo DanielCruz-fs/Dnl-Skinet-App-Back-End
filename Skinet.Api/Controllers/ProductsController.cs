6fc587a1c92e729be83a9029b1f15b3cb7f341d7
unknown <dannyfsociety@gmail.com>
2020-09-22 18:10:40 -0400

    Initial commit with connection to DB

---
+++ Skinet.Api/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Skinet.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "this is the list of all products";
+        }
+
+        [HttpGet("{id}")]
+        public object GetProductId(int id)
+        {
+            return new { Id = id, ProductTitle = "Pepper" };
+        }
+    }
+}
